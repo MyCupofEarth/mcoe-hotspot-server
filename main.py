83c7e7f1fce0eacb3ef581ce2b5fb3f19dc0dc04
MyCupofEarth <129629891+MyCupofEarth@users.noreply.github.com>
2026-08-15 19:57:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -124,10 +124,10 @@ def esim_provision(device_id: str):
     # Android/AOSP TS.48 test profile
     smdp_address = "prod.smdp-plus.rsp.goog"
 
-    matching_id = "YOUR_TEST_MATCHING_ID"
+    matching_id = "3TD6-8L82-HUE1-LVN6"
 
     activation_code = (
-        f"1${smdp_address}${matching_id}"
+        "1$prod.smdp-plus.rsp.goog$3TD6-8L82-HUE1-LVN6"
     )
 
     return {
